2a213ce6e2a8250b08671076d43db822bbff1022
Exequiel Lobaiza <exelob25@gmail.com>
2024-12-12 15:29:29 +0100

    starting

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <div className="container">
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <section>
+        <div className="status-container">
+          <h2>You win!</h2>
+          <p>Well done! 🎉</p>
+        </div>
+      </section>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
